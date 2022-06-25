7759e0246b0fc9c4e068511423e6db6bb02376d8
nickolae96 <nickolae.enachi@yandex.com>
2022-06-25 17:30:12 +0300

    added whitenoise to setting.py

--- todo/settings.py
+++ todo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,10 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+MEDIA_ROOT = BASE_DIR.joinpath('static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
